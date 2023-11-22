8e37a295f73ca099948b09f518d6297199aeee1c
Tom Conner <tomconner46@gmail.com>
2023-11-22 01:00:15 -0500

    wip

--- generator/content/confirm/stripe-return.js
+++ generator/content/confirm/stripe-return.js
@@ -30,4 +30,28 @@ document.addEventListener('DOMContentLoaded', async () => {
   window.parent.postMessage({
       'frameHeight': height
   }, 'https://dev.troop105treedrive.com');
-});
+  function showMessage(messageText) {
+    const messageContainer = document.querySelector("#payment-message");
+
+    messageContainer.classList.remove("hidden");
+    messageContainer.textContent = messageText;
+
+    setTimeout(function () {
+      messageContainer.classList.add("hidden");
+      messageContainer.textContent = "";
+    }, 4000);
+  }
+
+  // Show a spinner on payment submission
+  function setLoading(isLoading) {
+    if (isLoading) {
+      // Disable the button and show a spinner
+      document.querySelector("#submit").disabled = true;
+      document.querySelector("#spinner").classList.remove("hidden");
+      document.querySelector("#button-text").classList.add("hidden");
+    } else {
+      document.querySelector("#submit").disabled = false;
+      document.querySelector("#spinner").classList.add("hidden");
+      document.querySelector("#button-text").classList.remove("hidden");
+    }
+  }});
