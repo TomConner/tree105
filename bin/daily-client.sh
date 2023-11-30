f3a70fa9d8214ea84e79f8ffb151259889587056
Tom Conner <tomconner46@gmail.com>
2023-11-30 13:09:39 -0500

    daily

--- bin/daily-client.sh
+++ bin/daily-client.sh
@@ -9,11 +9,18 @@ get_file() {
     scp "$remote_path" "$local_path"
 }
 
+ssh "$ssh_remote" /home/tom/tree105/bin/daily-server.sh
+sleep 2
+
+echo "backup..."
 for file in $(ssh "$ssh_remote" /home/tom/tree105/bin/backup.sh); do
-    sleep 2
-    get_file "$file"
+    sleep 1
+
     echo "$file"
+    get_file "$file"
 done
+
+echo ""
 
 ls -lt "$local_path" | head -10
 
---
+++ bin/daily-server.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+db="/home/tom/tree105/db/tree105.sqlite"
+
+sqlite3 $db '.read /home/tom/tree105/bin/how_many.sql'
+sqlite3 $db '.read /home/tom/tree105/bin/active.sql'
