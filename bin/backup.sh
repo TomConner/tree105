c4e275953c45e85bb7909d0cc4b6063acb00adeb
Tom Conner <tom@tomconner.dev>
2023-11-30 14:30:06 +0000

    backup scripts

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,15 +6,25 @@ db_file="$HOME/tree105/db/$name.sqlite"
 backup_dir="$HOME"
 
 stamp="$(date -u +"%Y-%m-%d.%H-%M-%S")"
-db_backup_file="$backup_dir/$stamp-$name-db.sqlite"
-compose_full_backup_file="$backup_dir/$stamp-$name-compose-full.log"
-compose_backup_file="$backup_dir/$stamp-$name-compose.log"
 
+db_backup_file="$backup_dir/$stamp-$name-db.sqlite"
 sqlite3 "$db_file" ".backup $db_backup_file"
 echo "$db_backup_file"
 
+compose_full_backup_file="$backup_dir/$stamp-$name-compose-full.log"
 docker compose logs > "$compose_full_backup_file"
 echo "$compose_full_backup_file"
 
-docker compose logs --since 48h > "$log_backup_file"
-echo "$log_backup_file"
+compose_backup_file="$backup_dir/$stamp-$name-compose.log"
+docker compose logs --since 48h > "$compose_backup_file"
+echo "$compose_backup_file"
+
+docker_backup_file="$backup_dir/$stamp-$name-docker.log"
+docker events \
+    -f type=container \
+    -f type=service \
+    -f type=volume \
+    -f type=daemon \
+    -f type=network \
+    --since 48h --until 0s > "$docker_backup_file"
+echo "$docker_backup_file"
