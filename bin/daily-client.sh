7596ef8fb0effe3d7a74d03a9abd306b2cdadb80
Tom Conner <tomconner46@gmail.com>
2023-12-03 09:22:30 -0500

    daily

--- bin/daily-client.sh
+++ bin/daily-client.sh
@@ -14,7 +14,7 @@ sleep 2
 
 echo "backup..."
 for file in $(ssh "$ssh_remote" /home/tom/tree105/bin/backup.sh); do
-    sleep 1
+    sleep 2
 
     echo "$file"
     get_file "$file"
@@ -24,8 +24,15 @@ echo ""
 
 ls -lt "$local_path" | head -10
 
-echo "Enter when ready:"
+echo "Extract is next. Continue → Enter"
 read foo
 
 cd "$HOME/tree105/bin"
 ./extract.sh | less
+
+echo "Emails are next. Continue → Enter"
+read foo
+
+./emails sendem registered
+
+cd "$local_path"
