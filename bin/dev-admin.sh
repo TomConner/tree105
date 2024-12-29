7f50668ccb61ab81a75214784b0344583a93f1b8
Tom Conner <tom@tomconner.dev>
2024-12-29 15:50:00 +0000

    dev admin functions

--- bin/dev-admin.sh
+++ bin/dev-admin.sh
@@ -1,11 +1,18 @@
 #!/bin/zsh
 
-export local_tree="$HOME/tree105"
-export local_work="$local_tree/work"
+. "${HOME}/tree105/.env"
+echo Environment: ${ENV_LABEL:?}
+DB_FILE="${DB_DIR:?}/$(ls -t ${DB_DIR:?} | grep '\.sqlite' | head -1)"
+echo DB_FILE: ${DB_FILE:?}
 
+dev_admin() {
+    cd "${TREE_HOME}/bin"
+    . ./dev-admin.sh
+}
 pull_backup() {
     local ssh_remote="tom@troop105treedrive.com"
     local remote_tree="${ssh_remote}:tree105"
+    local local_work="${TREE_HOME}/work"
     mkdir -p $local_work
     ssh "$ssh_remote" "tree105/bin/daily-server.sh"
     scp "$remote_tree/work/tree105-backup.zip" "$local_work"
@@ -13,18 +20,24 @@ pull_backup() {
 }
 
 dev_front() {
-    cd "$local_tree/admin-ui"
+    cd "${TREE_HOME}/admin-ui"
     npm run dev
 }
 
-dev_back() {
-    cd "$local_tree/server"
+dev_server() {
+    cd "${TREE_HOME}/bin"
     ./server
 }
 
+tdb() {
+    sqlite3 ${DB_FILE:?}
+}
+
+which dev_admin
 which pull_backup
 which dev_front
-which dev_back
+which dev_server
+which tdb
 
 #./emails sendem registered
 
